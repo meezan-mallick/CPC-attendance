54b2a4c678ea8bbb083000f072b8a7b980bcf6d7
meezan-mallick <meezanmalek1234@gmail.com>
2025-03-11 07:31:36 +0530

    update college form fixed

--- app/Views/colleges/edit.php
+++ app/Views/colleges/edit.php
@@ -1,31 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
+<?= $this->extend('main') ?>
 
-<head>
-    <meta charset="UTF-8">
-    <meta charset="ISO-8859-1">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="/assets/css/dashboard.css">
+<?= $this->section('content') ?>
 
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-</head>
+<div class="container-fluid">
 
-<body>
+    <div class="m-4">
 
-
-    <?php include APPPATH . 'Views/navbar.php'; ?>
-
-    <div class="main-wrapper">
-
-        <?php include APPPATH . 'Views/header.php'; ?>
-
-
-
-        <div class="container add-form">
+        <div class=" add-form">
             <form action="<?= base_url('/colleges/update' . $college['id']) ?>" method="Post">
                 <div class="header">
-                    <a href="/colleges">Back to Colleges</a>
+                    <a class="btn btn-warning" href="/colleges">Back to Colleges</a>
                     <div>
                         <h2>Update College</h2>
 
@@ -36,62 +20,54 @@
                         </button>
                     </div>
                 </div>
+                <hr>
 
-                <!-- {/* PERSONAL DETAILS */} -->
-                <fieldset>
-                    <legend>Faculty Details</legend>
-                    <div class="form-container">
-                        <div class="row">
-                            <div>
-                                <label For="name">College Code</label>
+                <div class="form-container">
+                    <div class="row">
+                        <div class="col-12 col-md-6">
+                            <label For="name">College Code</label>
 
-                                <input
-                                    class="form-inputs"
-                                    type="text"
-                                    name="name"
-                                    id="name"
-                                    placeholder="Enter College Code"
-                                    value="<?= $college['college_code'] ?>" />
-                            </div>
-
-                            <div>
-                                <label For="username">College Name</label>
-
-                                <input
-                                    class="form-inputs"
-                                    type="text"
-                                    name="username"
-                                    id="username"
-                                    placeholder="Enter College Name"
-                                    value="<?= $college['college_name'] ?>" />
-                            </div>
-
-
+                            <input
+                                class="form-inputs"
+                                type="text"
+                                name="name"
+                                id="name"
+                                placeholder="Enter College Code"
+                                value="<?= $college['college_code'] ?>" />
                         </div>
 
-                        <?php
-                        if (isset($validation)) { ?>
-                            <div class="row" style="color: crimson;">
-                                <?= $validation->listErrors(); ?>
-                            </div><?php
-                                }
-                                if (isset($validationdup)) { ?>
-                            <div class="row" style="color: crimson;">
-                                <?= $validationdup; ?>
-                            </div><?php
-                                }
-                                    ?>
+                        <div class="col-12 col-md-6">
+                            <label For="username">College Name</label>
 
-                </fieldset>
+                            <input
+                                class="form-inputs"
+                                type="text"
+                                name="username"
+                                id="username"
+                                placeholder="Enter College Name"
+                                value="<?= $college['college_name'] ?>" />
+                        </div>
+
+
+                    </div>
+
+                    <?php
+                    if (isset($validation)) { ?>
+                        <div class="row" style="color: crimson;">
+                            <?= $validation->listErrors(); ?>
+                        </div><?php
+                            }
+                            if (isset($validationdup)) { ?>
+                        <div class="row" style="color: crimson;">
+                            <?= $validationdup; ?>
+                        </div><?php
+                            }
+                                ?>
 
             </form>
 
         </div>
 
     </div>
-
-
-
-</body>
-
-</html>
+</div>
+<?= $this->endSection() ?>
